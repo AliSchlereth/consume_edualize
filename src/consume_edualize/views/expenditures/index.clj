05560fc1a089b7b611c18614987c6ba3c3b864e8
Ali Schlereth <aschlere@sbcglobal.net>
2017-12-15 13:42:03 -0700

    Add display-index functionality. Not printing 404 any longer But also not printing content

--- src/consume_edualize/views/expenditures/index.clj
+++ src/consume_edualize/views/expenditures/index.clj
@@ -1,6 +1,18 @@
 (ns consume_edualize.views.expenditures.index
-  [:require [consume_edualize.views.layout :as layout]])
+  (:require [consume_edualize.views.layout :as layout]
+            [hiccup.core :refer [h]]
+            [ring.util.anti-forgery :as anti-forgery]))
+
+(defn display-by-state
+  [expenditures]
+  [:div {:class "all-states"}
+  (println expenditures)
+  (map
+    (fn [expenditure] [:h2 {:class "state"} (h (:body expenditure))])
+    expenditures)])
 
 (defn display-index
   [expenditures]
-  (println expenditures))
+  (layout/common "Edualize"
+                  [:div {:class "clear"}]
+                  (display-by-state expenditures)))
